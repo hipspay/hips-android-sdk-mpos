2561a76fb5e0a78f16ebedf074f7e335b4ca456b
Robert Czerwinski <robert@czerwinski.se>
2020-05-04 14:42:48 +0200

    Added Hips SDK dependencies

--- app/src/main/java/com/hips/sdk/android/hipsmposdemo/MainActivity.kt
+++ app/src/main/java/com/hips/sdk/android/hipsmposdemo/MainActivity.kt
@@ -1,12 +1,97 @@
 package com.hips.sdk.android.hipsmposdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hips.sdk.hips.common.exception.HipsException
+import com.hips.sdk.hips.common.model.CurrencyType
+import com.hips.sdk.hips.common.model.HipsTransactionResult
+import com.hips.sdk.hips.common.model.PriceDetails
+import com.hips.sdk.hips.common.result.HipsUiTransaction
+import com.hips.sdk.hips.ui.CallbackManager
+import com.hips.sdk.hips.ui.HipsUi
+import com.hips.sdk.hips.ui.HipsUiBuilder
+import com.hips.sdk.hips.ui.HipsUiCallback
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = this::class.java.simpleName
+
+    private lateinit var hipsUi: HipsUi
+
+    // Create a callbackManager instance
+    private val callbackManager = CallbackManager.Factory.create()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create instance of HipsUi
+        hipsUi = HipsUiBuilder()
+                .appContext(applicationContext)
+                .isTestMode(true)
+                .merchantId("YOUR_MERCHANT_ID")
+                .build()
+
+        // Register a callback to retrieve transaction results
+        hipsUi.registerCallback(callbackManager, object : HipsUiCallback<HipsTransactionResult> {
+            override fun onSuccess(hipsTransactionResult: HipsTransactionResult) {
+                Log.v(TAG, "onSuccess: $hipsTransactionResult")
+                main_title.text = "Success: $hipsTransactionResult"
+            }
+
+            override fun onCanceled() {
+                Log.v(TAG, "onCanceled")
+                main_title.text = "Transaction canceled"
+
+            }
+
+            override fun onError(
+                    exception: HipsException?,
+                    hipsUiTransactionRequest: HipsUiTransaction.Request?
+            ) {
+                Log.v(TAG, "onError: $exception - Initiating transaction request: $hipsUiTransactionRequest")
+                main_title.text = "Error: Transaction error ${exception?.message}"
+            }
+        })
+
+        settings_button.setOnClickListener {
+            hipsUi.openSettings(this)
+        }
+
+        payment_button.setOnClickListener {
+            hipsUi.startPayment(
+                    activity = this,
+
+                    /**
+                     * PriceDetails is nullable. If no object is passed Hips Ui i will provide controls
+                     * to fill in the required properties.
+                     *
+                     * Currently supported in SDK:
+                     * - Amount via Hips Keyboard, pass 0.0 to trigger keyboard
+                     * - Description
+                     *
+                     * In development:
+                     * - VAT
+                     * - Currency
+                     *
+                     */
+                    //
+                    priceDetails = PriceDetails(
+                            amount = 0.0, // Pass zero to trigger Hips Ui Keyboard
+                            vat = 0.0,
+                            description = "This is a test payment",
+                            currencyType = CurrencyType.USDollar
+                    ),
+                    isOfflinePayment = false
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
